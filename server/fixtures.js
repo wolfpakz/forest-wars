b945027f02e0068d86e56d511e70486e4385f30e
Dan Porter <wolfpakz@gmail.com>
2014-11-14 19:11:22 -0800

    Only render a cell green if it's a tree

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,7 +1,6 @@
 Meteor.startup(function () {
   if (Boards.find().count() === 0) {
     // Create an initial board for development purposes
-
     Boards.insert(ForestWars.generateRectangularBoard(20, 20));
   }
 });
